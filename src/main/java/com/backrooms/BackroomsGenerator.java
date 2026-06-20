48aa3d5f6b5e0e48b31f18a4f5dab62146f36752
Iam8lu3 <a.dark.shaolin2gmail.com>
2026-06-20 10:11:55 -0400

    Cleaned up project structure and optimized world generator

--- src/main/java/com/backrooms/BackroomsGenerator.java
+++ src/main/java/com/backrooms/BackroomsGenerator.java
@@ -1,90 +1,96 @@
 package com.backrooms;
 
-import org.bukkit.NamespacedKey;
-import org.bukkit.World;
-import org.bukkit.block.BlockState;
-import org.bukkit.block.Container;
-import org.bukkit.entity.EntityType;
+import org.bukkit.Material;
 import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.generator.BlockPopulator;
-import org.bukkit.generator.LimitedRegion;
 import org.bukkit.generator.WorldInfo;
-import org.bukkit.loot.Lootable;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 public class BackroomsGenerator extends ChunkGenerator {
 
-    @Override
-    public void generateNoise(@NotNull WorldInfo worldInfo, @NotNull Random random, int x, int z, @NotNull ChunkData chunkData) { }
+    private static final int LAYER_HEIGHT = 6; 
+    private static final int FLOOR_THICKNESS = 2; 
 
     @Override
-    public void generateSurface(@NotNull WorldInfo worldInfo, @NotNull Random random, int x, int z, @NotNull ChunkData chunkData) { }
+    public void generateNoise(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ, @NotNull ChunkData chunkData) {
+        long seed = worldInfo.getSeed();
+        int minY = worldInfo.getMinHeight();
+        int maxY = worldInfo.getMaxHeight();
 
-    @Override
-    public void generateBedrock(@NotNull WorldInfo worldInfo, @NotNull Random random, int x, int z, @NotNull ChunkData chunkData) { }
+        boolean isSpawnChunk = (chunkX == 0 && chunkZ == 0);
 
-    @Override
-    public @NotNull List<BlockPopulator> getDefaultPopulators(@NotNull World world) {
-        return Collections.singletonList(new BlockPopulator() {
-            @Override
-            public void populate(@NotNull WorldInfo worldInfo, @NotNull Random random, int x, int z, @NotNull LimitedRegion limitedRegion) {
-                BackroomsPlugin plugin = BackroomsPlugin.getInstance();
-                int rotation = plugin.getTileSelectionService().getRandomRotation();
-                
-                org.bukkit.World bukkitWorld = org.bukkit.Bukkit.getWorld(worldInfo.getUID());
-                if (bukkitWorld == null) return;
+        // Deterministic seed for the 5% rare resource vault roll
+        long chunkSeed = (long) chunkX * 341873128712L + (long) chunkZ * 132897987541L ^ seed;
+        Random chunkRand = new Random(chunkSeed);
+        boolean isOreCoreChunk = !isSpawnChunk && (chunkRand.nextInt(100) < 5);
 
-                // 1. Paste the physical tile layout
-                plugin.getSchematicPasteService().pasteChunkTile(bukkitWorld, x, z, rotation);
+        for (int y = minY; y < maxY - (LAYER_HEIGHT + FLOOR_THICKNESS); y += (LAYER_HEIGHT + FLOOR_THICKNESS)) {
+            int floorY1 = y;
+            int floorY2 = y + 1;
+            int ceilingY1 = y + LAYER_HEIGHT;
+            int ceilingY2 = y + LAYER_HEIGHT + 1;
 
-                // 2. Scan the chunk snapshot for containers to apply loot tables
-                int minX = x << 4;
-                int minZ = z << 4;
-                int maxX = minX + 15;
-                int maxZ = minZ + 15;
+            for (int localX = 0; localX < 16; localX++) {
+                for (int localZ = 0; localZ < 16; localZ++) {
 
-                for (int blockX = minX; blockX <= maxX; blockX++) {
-                    for (int blockZ = minZ; blockZ <= maxZ; blockZ++) {
-                        for (int blockY = bukkitWorld.getMinHeight(); blockY < bukkitWorld.getMaxHeight(); blockY++) {
-                            
-                            BlockState state = limitedRegion.getBlockState(blockX, blockY, blockZ);
-                            
-                            if (state instanceof Container && state instanceof Lootable lootable) {
-                                String typeStr = state.getType().toString();
-                                NamespacedKey lootKey;
+                    // Floors
+                    chunkData.setBlock(localX, floorY1, localZ, Material.LIGHT_GRAY_CONCRETE);
+                    chunkData.setBlock(localX, floorY2, localZ, Material.LIGHT_GRAY_CONCRETE);
 
-                                // Separate food cookers from standard storage units
-                                if (typeStr.contains("SMOKER") || typeStr.contains("FURNACE")) {
-                                    lootKey = new NamespacedKey(plugin, "backrooms/kitchen");
-                                } else {
-                                    lootKey = new NamespacedKey(plugin, "backrooms/containers");
-                                }
+                    // Ceilings
+                    chunkData.setBlock(localX, ceilingY1, localZ, Material.LIGHT_GRAY_CONCRETE);
+                    chunkData.setBlock(localX, ceilingY2, localZ, Material.LIGHT_GRAY_CONCRETE);
 
-                                lootable.setLootTable(org.bukkit.Bukkit.getLootTable(lootKey));
-                                state.update(true, false);
+                    // Fluorescent Lights
+                    if (localX % 4 == 0 && localZ % 4 == 0) {
+                        chunkData.setBlock(localX, ceilingY1, localZ, Material.PEARLESCENT_FROGLIGHT);
+                    }
+
+                    // Room Volume Fill
+                    for (int roomY = floorY2 + 1; roomY < ceilingY1; roomY++) {
+                        if (isSpawnChunk) {
+                            if (localX == 0 || localX == 15 || localZ == 0 || localZ == 15) {
+                                chunkData.setBlock(localX, roomY, localZ, Material.YELLOW_TERRACOTTA);
+                            } else {
+                                chunkData.setBlock(localX, roomY, localZ, Material.AIR);
                             }
+                            continue;
+                        }
+
+                        if (isOreCoreChunk) {
+                            if (localX == 0 || localX == 15 || localZ == 0 || localZ == 15) {
+                                chunkData.setBlock(localX, roomY, localZ, Material.YELLOW_TERRACOTTA);
+                            } else {
+                                int oreRoll = chunkRand.nextInt(100);
+                                if (oreRoll < 2) {
+                                    chunkData.setBlock(localX, roomY, localZ, Material.DIAMOND_ORE);
+                                } else if (oreRoll < 10) {
+                                    chunkData.setBlock(localX, roomY, localZ, Material.GOLD_ORE);
+                                } else if (oreRoll < 25) {
+                                    chunkData.setBlock(localX, roomY, localZ, Material.IRON_ORE);
+                                } else {
+                                    chunkData.setBlock(localX, roomY, localZ, Material.COBBLESTONE);
+                                }
+                            }
+                            continue;
+                        }
+
+                        // Standard layout
+                        if (localX == 0 || localX == 15 || localZ == 0 || localZ == 15) {
+                            chunkData.setBlock(localX, roomY, localZ, Material.YELLOW_TERRACOTTA);
+                        } else {
+                            chunkData.setBlock(localX, roomY, localZ, Material.AIR);
                         }
                     }
                 }
-
-                // 3. Low-frequency neutral mob spawning (Ominous passive environment)
-                if (random.nextInt(100) < 15) { // 15% chance per chunk execution zone
-                    int spawnX = minX + random.nextInt(16);
-                    int spawnZ = minZ + random.nextInt(16);
-                    int spawnY = bukkitWorld.getMinHeight() + random.nextInt(bukkitWorld.getMaxHeight() - bukkitWorld.getMinHeight());
-
-                    if (limitedRegion.getType(spawnX, spawnY, spawnZ).isAir() && 
-                        limitedRegion.getType(spawnX, spawnY - 1, spawnZ).isSolid()) {
-                        
-                        EntityType type = random.nextBoolean() ? EntityType.SPIDER : EntityType.ENDERMAN;
-                        bukkitWorld.spawnEntity(new org.bukkit.Location(bukkitWorld, spawnX, spawnY, spawnZ), type);
-                    }
-                }
             }
-        });
+        }
     }
+
+    @Override
+    public void generateSurface(@NotNull WorldInfo worldInfo, @NotNull Random random, int x, int z, @NotNull ChunkData chunkData) {}
+
+    @Override
+    public void generateBedrock(@NotNull WorldInfo worldInfo, @NotNull Random random, int x, int z, @NotNull ChunkData chunkData) {}
 }
--- src/main/java/com/backrooms/BackroomsPlugin.java
+++ src/main/java/com/backrooms/BackroomsPlugin.java
@@ -1,7 +1,5 @@
 package com.backrooms;
 
-import com.backrooms.services.SchematicPasteService;
-import com.backrooms.services.TileSelectionService;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -10,19 +8,15 @@ import org.jetbrains.annotations.Nullable;
 public class BackroomsPlugin extends JavaPlugin {
 
     private static BackroomsPlugin instance;
-    private SchematicPasteService schematicPasteService;
-    private TileSelectionService tileSelectionService;
 
     @Override
     public void onEnable() {
         instance = this;
-        this.schematicPasteService = new SchematicPasteService(this);
-        this.tileSelectionService = new TileSelectionService(this);
     }
 
-    public static BackroomsPlugin getInstance() { return instance; }
-    public SchematicPasteService getSchematicPasteService() { return schematicPasteService; }
-    public TileSelectionService getTileSelectionService() { return tileSelectionService; }
+    public static BackroomsPlugin getInstance() { 
+        return instance; 
+    }
 
     @Override
     public @Nullable ChunkGenerator getDefaultWorldGenerator(@NotNull String worldName, @Nullable String id) {
